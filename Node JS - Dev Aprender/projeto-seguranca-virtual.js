a46300dbcae4c09103cb017e69ca08a8e469bf83
Jefferson Pedro <jeffersonpedro05@gmail.com>
2021-09-03 20:59:21 -0300

    Projeto Finalizado

--- Node JS - Dev Aprender/projeto-seguranca-virtual.js
+++ Node JS - Dev Aprender/projeto-seguranca-virtual.js
@@ -4,6 +4,7 @@ let botao = document.getElementById('btn');
 let p = document.querySelector('#pesquisa');
 
 const convidadosDoCristian = ['Mariana', 'Marlaine', 'Vanessa'];
+
 const convidadosDoCarlos = ['Dirley', 'Renan', 'Jefferson'];
 
 botao.addEventListener('click', clicar);
@@ -11,14 +12,20 @@ function clicar() {
 
     if ((nome.value.length == '') || (convite.value.length == '')) {
         alert('Preencha os campos para continuar!');
-    } else if (convite.value == 'Cristian') {
-        // alert('até aqui, tudo bem!');
-        for (let i of convidadosDoCristian) {
-            if (i == nome.value) {
-                p.innerHTML = '<strong>Você está liberado para entrar! Aproveite a festa.</strong>';
-            } else {
-                //p.innerHTML = '<strong>Você não foi convidado para a festa,por isso não pode entrar!<strong>';
-            }
+    } else if (convite.value === 'Cristian') {
+        if (convidadosDoCristian.includes(nome.value)) {
+            p.innerHTML = '<strong>Você está liberado para entrar! Aproveite a festa.</strong>';
+        }
+        else {
+            p.innerHTML = '<strong>Você não foi convidado para a festa,por isso não pode entrar!<strong>';
+        }
+    } else if (convite.value === 'Carlos') {
+        if (convidadosDoCarlos.includes(nome.value)) {
+            p.innerHTML = '<strong>Você está liberado para entrar! Aproveite a festa.</strong>';
+        }
+        else {
+            p.innerHTML = '<strong>Você não foi convidado para a festa,por isso não pode entrar!<strong>';
         }
     }
-};
+}
+
